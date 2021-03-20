7bc901d90422d92de40b9781deb6910ed4bdf000
Duality <jramirezarroyo21@gmail.com>
2021-03-19 23:31:49 -0700

    question box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
-var questions = {
+
+var timerEL = document.querySelector("#time");
+var startingButton = document.querySelector("#starter");
+var startingIntro = document.querySelector("startingPage");
+
+
+
+
+
+
+var questions = [{
+    question: "How do you write comments in Javascript?",
+    options: ["//", "/*", "<!---->", "None of Above"],
+    answers: "//"
     
+},
+{
+    question: "Which one of these data types stores a single true or false value?",
+    options: ["string", "object", "float", "boolean"],
+    answers: "boolean"
+    
+},
+{
+    question:"Which ones of these values is not considered false?",
+    options:["''", "null", "'0'", "0"],
+    answers:"'0'"
+    
+},
+{
+    question:"What does CSS stand for?",
+    options:["Common Style Sheet", "Cascading Style Sheet", "Computer Style Sheet", "Colorful Style Sheet"],
+    answers: "Cascading Style Sheet"
+    
+},
+{
+    question:"What does HTMl stand for?",
+    options: ["Hyper Text Marketing Language", "Hyper Text Management Language", "Hyper Text Multiple Language", "Hyper Text Markup Language"],
+    answers: "Hyper Text Markup Language"
+    
+},
+
+function startQuiz() {
+
+
+
+
 }
+
+
